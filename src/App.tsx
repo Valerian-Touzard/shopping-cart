e0e0fdb672baf39fdbec64bde84da3651d791180
vtouzard <vtouzard244@gmail.com>
2024-03-13 11:52:25 +0100

    feat: Ajout de la navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import Cart from "./pages/cart/cart";
+import Header from "./components/header/header";
 
 const App = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
