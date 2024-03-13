0530c7b18c2bb796af8ed0d6e82498ee017da88a
vtouzard <vtouzard244@gmail.com>
2024-03-13 12:00:38 +0100

    feat: Ajout de l'appel API pour récupérer les informations

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Circles } from "react-loader-spinner";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchListOfProducts();
+  },[]);
+
+  const fetchListOfProducts = async () => {
+    setLoading(true);
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+
+    if (data) {
+      setLoading(false);
+      setProducts(data);
+    }
+  };
+  return (
+    <div>
+      {loading ? 
+        <div className="min-h-screen w-full flex justify-center items-center">
+          <Circles height={'120'} width={'120'} color="rgb(127,29,29)" visible={true} />
+        </div>
+      : null}
+    </div>
+  );
 };
 
 export default Home;
