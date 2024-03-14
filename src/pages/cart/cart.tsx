9d0864f2371f0f8bfb1325f5acacd47c47c8d790
vtouzard <vtouzard244@gmail.com>
2024-03-14 16:43:02 +0100

    feat: Ajout de la gestion du panier pour modifier le total(ajout seulement)

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootStateStore } from "../../store";
+import { Product } from "../home/home";
 
 const Cart = () => {
+
+  const [total, setTotal] = useState(0) 
+
+  const {cart} = useSelector((state : RootStateStore) => state);
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc: number, curr: Product) => acc + curr.price, 0))
+  }, [])
+
+  console.log(cart, total);
+  
+
   return <div>Cart</div>;
 };
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './slices/cart-slice';
 
+interface RootState {
+    cart: any; // Définissez un type approprié pour votre état de panier
+  }
+
 const store = configureStore({
     reducer: {
         cart: cartReducer,
@@ -8,3 +12,6 @@ const store = configureStore({
 })
 
 export default store
+
+export type AppDispatch = typeof store.dispatch;
+export type RootStateStore = ReturnType<typeof store.getState>;
